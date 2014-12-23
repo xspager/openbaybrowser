a494e8a54e7ded165e059707407c7653464c68ed
Daniel Lemos <xspager@gmail.com>
2014-12-23 20:40:58 -0200

    Show file size on torrent admin interface list and code of a filed atempt of assembling the magnet url for the torrents

--- torrent/admin.py
+++ torrent/admin.py
@@ -1,15 +1,37 @@
+import urllib
+
 from django.contrib import admin
 from torrent.models import Torrent
+
+from django.template.defaultfilters import filesizeformat
 
 class TorrentAdmin(admin.ModelAdmin):
 
     search_fields = ['name']
 
     def torrent_url(self, obj):
-        return '<a href="http://torcache.net/torrent/%s.torrent">get torrent</a>' % obj.hash
+        return '<a href="http://torcache.net/torrent/%s.torrent" target="_blank">get torrent</a>' % obj.hash.upper()
     torrent_url.allow_tags=True
 
+    def file_size(self, obj):
+       return filesizeformat(obj.size)
 
-TorrentAdmin.list_display += ('torrent_url',)
+    #def magnet_url(self, obj):
+    #    
+    #    return '<a href="magnet:?xt=urn:btih:%(hash)s&amp;%(params)s" target="_blank">magenet link</a>' % {
+    #        'hash': obj.hash.upper(),
+    #        'params': urllib.urlencode({
+    #            'dn': obj.name.encode('utf-8'),
+    #            'xl': obj.size,
+    #            'dl': obj.size
+    #        })
+    #    }
+
+        
+    #magnet_url.allow_tags=True
+    torrent_url.allow_tags=True
+
+#TorrentAdmin.list_display += ('file_size', 'magnet_url', 'torrent_url')
+TorrentAdmin.list_display += ('file_size', 'torrent_url')
 
 admin.site.register(Torrent, TorrentAdmin)
